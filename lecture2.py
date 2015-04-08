154da36815dcf82eca7540cda2da2f20fafcddfe
lhyqie <lhymvp@gmail.com>
2015-04-08 13:53:26 -0500

    Add lecture 2 SVD

---
+++ lecture2.py
@@ -0,0 +1,22 @@
+import numpy as np
+la = np.linalg
+words = ["I", "like", "enjoy", "deep", "learning", "NLP", "flying", "."]
+
+X = np.array([[0,2,1,0,0,0,0,0],
+              [2,0,0,1,0,1,0,0],
+              [1,0,0,0,0,0,1,0],
+              [0,1,0,0,1,0,0,0],
+              [0,0,0,1,0,0,0,1],
+              [0,1,0,0,0,0,0,1],
+              [0,0,1,0,0,0,0,1],
+              [0,0,0,0,1,1,1,0]])
+
+U, s, Vh = la.svd(X, full_matrices = False) 
+
+import matplotlib.pyplot as plt
+
+for i in xrange(len(words)):
+    plt.text(U[i,0], U[i,1], words[i])
+plt.xlim([-0.8,0.8])
+plt.ylim([-0.8,0.8])
+plt.show()
